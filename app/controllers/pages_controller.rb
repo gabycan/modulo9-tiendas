ab3e027c09654940538e26b4053fa7684db7ffdb
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-11-10 20:42:02 -0300

    agreando la UI para la vista de home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,40 @@
 class PagesController < ApplicationController
+
+  # def home
+  #     cookies[:mi_primera_cookie] = "Hola mundo"
+  # end
+
   def home
-    cookies[:mi_primera_cookie] = "Hola mundo"
+      if cookies.signed[:mi_primera_cookie]
+          puts cookies.signed[:mi_primera_cookie]
+      else
+          cookies.signed[:mi_primera_cookie] = "Hola mundo FIRMADO 🍕"
+      end
+      
+      if cookies.encrypted[:encriptada]
+          cookies.encrypted[:encriptada]
+      else
+          cookies.encrypted[:encriptada] = "TEXTO SECRETO 🧀"
+      end
   end
+
+  def home
+      #1. Preguntar si existe la variable cookies[:cart_id]
+      if cookies[cart_id]
+          #si existe: buscar el registro de cart según el cookie
+          #definir una variable @cart
+          @cart = Cart.find(cookies[:card_id])
+      else
+        #si no existe: crear un registro en cart
+        #asignar la cookie con el id
+        @cart = Cart.create(total: 0)
+        cookies[:cart_id] = @cart.id
+      end
+
+      #2. mostrar la cantidad de productos en el carrito
+      
+      #@products = Product.all.shuffle
+      @products = Product.all
+  end
+  
 end
