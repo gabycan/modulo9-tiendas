60cec6ec72a6f3e491007328d364624fc9ea8fa4
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-11-10 20:02:21 -0300

    agregando coookies para el carrito

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,5 +1,23 @@
 class PagesController < ApplicationController
 
+    # def home
+    #     cookies[:mi_primera_cookie] = "Hola mundo"
+    # end
+
+    def home
+        if cookies.signed[:mi_primera_cookie]
+            puts cookies.signed[:mi_primera_cookie]
+        else
+            cookies.signed[:mi_primera_cookie] = "Hola mundo FIRMADO 🍕"
+        end
+        
+        if cookies.encrypted[:encriptada]
+            cookies.encrypted[:encriptada]
+        else
+            cookies.encrypted[:encriptada] = "TEXTO SECRETO 🧀"
+        end
+    end
+
     def home
         #1. Preguntar si existe la variable cookies[:cart_id]
         if cookies[cart_id]
