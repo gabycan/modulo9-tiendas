89ffcd61ec251c4200908fd612760c4f953eacc9
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-11-17 13:01:38 -0300

    agregando cookies para el carrito

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,49 +1,20 @@
 class PagesController < ApplicationController
-  # def home
-  #     cookies[:mi_primera_cookie] = "Hola mundo"
-  # end
-    
-  def home
-      
-      if cookies[:plana]
-          puts cookies[:plana]
-      else
-          cookies[:plana] = "COOKIE PLANA 🧀"
-      end
-
-      if cookies.signed[:firmada]
-          puts cookies.signed[:firmada]
-      else
-          cookies.signed[:firmada] = "Hola mundo FIRMADO 🍕"
-      end
-      
-      if cookies.encrypted[:encriptada]
-          cookies.encrypted[:encriptada]
-      else
-          cookies.encrypted[:encriptada] = "TEXTO SECRETO 🧀"
-      end
-
-      if session[:variable_sesion]
-        puts session[:variable_sesion]
-      else
-        session[:variable_session] = "EN LA SESION 🥓"
-      end
-    end
-
+  
   def home
       #1. Preguntar si existe la variable cookies[:cart_id]
       if cookies[cart_id]
-          #si existe: buscar el registro de cart según el cookie
+          #si existe: buscar el registro de Cart según el cookies[:cart_id]
           #definir una variable @cart
-          @cart = Cart.find(cookies[:card_id])
+          @cart = Cart.find(cookies[:cart_id])
       else
-        #si no existe: crear un registro en cart
+        #si no existe: crear un registro en Cart
         #asignar la cookie con el id
         @cart = Cart.create(total: 0)
         cookies[:cart_id] = @cart.id
       end
-
-      #2. mostrar la cantidad de productos en el carrito
+      #cookies.delete(:cart_id)
+      
+      #2. Mostrar la cantidad de productos en el carrito
       
       #@products = Product.all.shuffle
       @products = Product.all
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,32 @@
 module PagesHelper
+    # def home
+    #     cookies[:mi_primera_cookie] = "Hola mundo"
+    # end
+    
+    def cookies_handler
+        if cookies[:plana]
+            puts cookies[:plana]
+        else
+            cookies[:plana] = "COOKIE PLANA 🧀"
+        end
+  
+        if cookies.signed[:firmada]
+            puts cookies.signed[:firmada]
+        else
+            cookies.signed[:firmada] = "Hola mundo FIRMADO 🍕"
+        end
+        
+        if cookies.encrypted[:encriptada]
+            cookies.encrypted[:encriptada]
+        else
+            cookies.encrypted[:encriptada] = "TEXTO SECRETO 🧀"
+        end
+  
+        if session[:variable_sesion]
+          puts session[:variable_sesion]
+        else
+          session[:variable_session] = "EN LA SESION 🥓"
+        end
+  
+    end
 end
