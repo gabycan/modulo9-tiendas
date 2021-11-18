aeb10ad2bc17d7556aaa9a250f6fea25c94999cc
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-11-17 21:02:16 -0300

    agregando la base de las pruebas

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   
   describe '#create' do
-    it
+    it 'when attributes are not valid' do
+    #Arrange | Arreglar | Preparar
+    #Act | Actuar | Ejecutar
+    #Assert | Afirmar | Validar
+    end
   end
 end
