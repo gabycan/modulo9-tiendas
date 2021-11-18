124b9a9f7a2d201314cb3986d2b2325dbbbe6bed
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-11-17 21:37:44 -0300

    terminando la prueba de category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Category, type: :model do
   describe '#create' do
     it 'when attributes are not valid' do
     #Arrange | Arreglar | Preparar
+    new_category = Category.new
     #Act | Actuar | Ejecutar
+    new_category.save
     #Assert | Afirmar | Validar
+    expect(new_category.errors[:name].count).to_not be(0)
     end
   end
 end
